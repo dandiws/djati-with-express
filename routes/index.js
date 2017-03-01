315dceda3baff61c7e5a18a20a43c1a1b7d3f24a
Dandi Wiratsangka <dandiwiratsangka@gmail.com>
2017-03-01 20:01:52 +0700

    intial commit

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('demo');
+});
+router.get('/about', function(req, res, next) {
+  res.render('about');
+});
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+module.exports = router;
