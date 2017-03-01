fec245e2a9ef18d7a6c5979b60bd00f7e3d9d1a6
Dandi Wiratsangka <dandiwiratsangka@gmail.com>
2017-03-01 20:38:14 +0700

    fix for show

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('demo');
+  res.render('index', { title : 'Welcome'});
 });
 router.get('/about', function(req, res, next) {
-  res.render('about');
+  res.render('about', { title : 'About'});
 });
 router.get('/contact', function(req, res, next) {
-  res.render('contact');
+  res.render('contact', { title : 'Contact'});
 });
 module.exports = router;
